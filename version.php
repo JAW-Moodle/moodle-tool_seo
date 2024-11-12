50d4fd0b3767e42d8b7b8b1b5e5a2e516b1e2e4b
Jakob Heinemann <jakob@jakobheinemann.de>
2024-11-12 09:43:08 +0100

    supports Moodle 4.5 onwards

--- version.php
+++ version.php
@@ -29,4 +29,4 @@ $plugin->release = 2024111200; // Match release exactly to version.
 $plugin->requires = 2024100700; // Moodle 4.5.
 $plugin->component = 'tool_seo';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->supported = [405];     // Supports Moodle 4.1 or later.
+$plugin->supported = [405,409];     // Supports Moodle 4.5 or later.
